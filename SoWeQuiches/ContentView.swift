83752468102110de613d43db29f0158a13375ac1
Nicolas Barbosa <nicolas.barbosa@orange.com>
2022-02-14 10:45:37 +0100

    Little words for laroche

--- SoWeQuiches/ContentView.swift
+++ SoWeQuiches/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Laroche suce des bites en enfer")
             .padding()
     }
 }
