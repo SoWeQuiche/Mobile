0c5e18e543d05b33e19d49d87e7e2d41f7b70544
Nicolas Barbosa <nicolas.barbosa@orange.com>
2022-02-14 10:25:40 +0100

    Initial Commit

---
+++ SoWeQuiches/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoWeQuiches
+//
+//  Created by Nicolas Barbosa on 14/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SoWeQuiches/SoWeQuichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoWeQuichesApp.swift
+//  SoWeQuiches
+//
+//  Created by Nicolas Barbosa on 14/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SoWeQuichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
