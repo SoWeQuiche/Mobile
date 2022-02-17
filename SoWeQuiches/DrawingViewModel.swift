10cad273a3f49b4af7f12709a288d6b4c37ec58b
MaxenceMottard <maxence.mottard@worldline.com>
2022-02-17 19:39:46 +0100

    clean code

--- SoWeQuiches/DrawingViewModel.swift
+++ SoWeQuiches/DrawingViewModel.swift
@@ -29,24 +29,10 @@ class DrawingViewModel: ObservableObject {
 }
 
 extension PKCanvasView {
-
     func saveAsUIImage() -> UIImage {
         let drawing = self.drawing
         let visibleRect = self.bounds
         let image = drawing.image(from: visibleRect, scale: UIScreen.main.scale)
         return image
-    }
-}
-
-
-
-private class JWTNetworkRequestInterceptor: NetworkRequestInterceptor {
-//    let userService = UserService()
-
-    func intercept(_ request: inout URLRequest) async throws {
-//        guard let token = try? await userService.getMe.call() else {
-//                throw NetworkError.custom("NO_AVAILABLE_TOKEN")
-//        }
-        request.addValue("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjBlMjUwNDllMGE0MDQ0NWMxNjRlNDAiLCJpYXQiOjE2NDUwOTQxNzYsImV4cCI6MTY2MDY0NjE3Nn0.Cu2n46SdP1aG4h2FyYyvPS825o56xRKCpYpNEPaQPQ8", forHTTPHeaderField: "authorization")
     }
 }
