34d37df2444c8e22048ab518625392e09352a28d
Zakarya Tolba <zakarya.tolba@ynov.com>
2022-03-08 14:11:25 +0100

    Removed navigation view from DrawingView

--- SoWeQuiches/Views/DrawingView.swift
+++ SoWeQuiches/Views/DrawingView.swift
@@ -14,44 +14,42 @@ struct DrawingView: View {
     @Binding var isPresenting: Bool
 
     var body: some View {
-        NavigationView {
-            VStack(alignment: .center, spacing: 0) {
-                Text(viewModel.attendance.name)
-                    .foregroundColor(Color.white)
-                    .font(.title)
-                    .bold()
+        VStack(alignment: .center) {
+            Text(viewModel.attendance.name)
+                .foregroundColor(Color.white)
+                .font(.title)
+                .bold()
 
-                Text(viewModel.attendance.timeslot)
-                    .foregroundColor(Color.white)
-                    .font(.title2)
+            Text(viewModel.attendance.timeslot)
+                .foregroundColor(Color.white)
+                .font(.title2)
 
-
-                VStack {
-                    CanvasView(canvasView: $viewModel.canvasView)
-                }
-                .frame(maxWidth: .infinity, maxHeight: 400)
-
-                Spacer()
-
-                Button(action: {
-                    Task {
-                        await viewModel.saveSignature()
-                        isPresenting = false
-                    }
-                }) {
-                    Text("Valider ma présence")
-                        .frame(maxWidth: .infinity)
-                        .padding(.vertical)
-                }
-                .foregroundColor(.white)
-                .background(Color("orange"))
-                .cornerRadius(50)
-
-                Spacer()
+            Spacer()
+            VStack {
+                CanvasView(canvasView: $viewModel.canvasView)
             }
-            .padding()
-            .background(Color("background"))
+            .frame(maxWidth: .infinity, maxHeight: 400)
+
+            Spacer()
+
+            Button(action: {
+                Task {
+                    await viewModel.saveSignature()
+                    isPresenting = false
+                }
+            }) {
+                Text("Valider ma présence")
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical)
+            }
+            .foregroundColor(.white)
+            .background(Color("orange"))
+            .cornerRadius(50)
+
+            Spacer()
         }
+        .padding()
+        .background(Color("background"))
     }
 }
 
