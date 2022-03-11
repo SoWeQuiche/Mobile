080171dd499af3d1711b659362e40026e6b9aab9
Zakarya TOLBA <45199202+zaktolba@users.noreply.github.com>
2022-03-11 15:38:47 +0100

    Feature/design fixes (#30)
    * Redesigned disconnect button & added confirm modal
    
    * Added logo on login page
    
    * Added paddings
    
    * Force portrait orientation for the app

--- SoWeQuiches/Views/HomeView.swift
+++ SoWeQuiches/Views/HomeView.swift
@@ -14,6 +14,7 @@ struct HomeView: View {
     @State var selectedAttendance: Attendance?
     @State var nextTimeSlots: [Timeslot] = []
     @State var actualTimeSlot: Timeslot?
+    @State var showDisconnectAlert: Bool = false
 
     let userService: UserService = UserService()
     var timeslotService: TimeslotService = TimeslotService()
@@ -23,11 +24,23 @@ struct HomeView: View {
         NavigationView {
             ScrollView {
                 VStack {
-                    Text(actualTimeSlot != nil ? actualTimeSlot?.groupName ?? "" : "Aucun séance prévu")
-                        .font(.title)
-                        .foregroundColor(Color.white)
-                        .padding(.vertical, 30)
-
+                    ZStack {
+                        Text(actualTimeSlot != nil ? actualTimeSlot?.groupName ?? "" : "Aucun séance prévu")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .padding(.vertical, 65)
+                        Button(action: { self.showDisconnectAlert.toggle() }) {
+                            Image(systemName: "power")
+                                .font(Font.subheadline.weight(.bold))
+                        }
+                        .padding(10)
+                        .foregroundColor(.white)
+                        .background(Color("orange"))
+                        .clipShape(Circle())
+                        .padding(.horizontal, 30)
+                        .padding(.bottom, 25)
+                        .position(x: 340, y: 50)
+                    }
                     VStack(spacing: 50) {
                         VStack {
                             Text(actualTimeSlot?.groupName ?? "")
@@ -46,6 +59,7 @@ struct HomeView: View {
                             HStack {
                                 Button(action: { selectedAttendance = Attendance(name: actualTimeSlot?.groupName ?? "", timeslot: actualTimeSlot?.courseTimelapse ?? "") }) {
                                     Text("Signer")
+                                        .bold()
                                         .foregroundColor(Color.white)
                                         .frame(maxWidth: .infinity, maxHeight: 16)
                                         .padding(.vertical)
@@ -99,12 +113,6 @@ struct HomeView: View {
                                     }
                                 }
                             }
-
-                            Button("Disconnect") {
-                                Task {
-                                    await disconnect()
-                                }
-                            }
                         }
                     }
                 }
@@ -115,6 +123,16 @@ struct HomeView: View {
             .navigationBarHidden(true)
             .task {
                 await fetchUserTimeslots()
+            }
+            .alert(isPresented: $showDisconnectAlert) {
+                Alert(title: Text("Déconnexion"),
+                 message: Text("Êtes-vous sûr(e) de vouloir vous déconnecter?"),
+                 primaryButton: .cancel(Text("Annuler")),
+                 secondaryButton: .destructive(Text("Se déconnecter"), action: {
+                    Task {
+                        await disconnect()
+                    }
+                 }))
             }
         }
     }
